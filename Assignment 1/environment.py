947fe8adf6b74b9472cb43df6e1b23ff8d4d25e6
aryan1602 <sunaider21@gmail.com>
2024-02-27 16:55:39 +0530

    made chages

--- Assignment 1/environment.py
+++ Assignment 1/environment.py
@@ -13,49 +13,50 @@ class GridWorld:
         self.prob_stay = 0.1
         self.prob_rleft = 0.05
         self.prob_rright = 0.05
+        self.gamma = gamma
  
     def next_action(self, policy):
         return np.random.choice([0, 1, 2, 3], p=policy[self.state])
 
-    def next_state(self):
+    def next_state(self, current_state, act):
         prob = np.random.uniform()
         if(prob < 0.05):
-            self.action = (self.action + 3)%4
+            act = (act + 3)%4
         elif(prob < 0.1):
-            self.action = (self.action + 1)%4
+            act = (act + 1)%4
         elif(prob < 0.2):
-            self.action = None
-        new_state = self.state
-        if(self.action == None):
+            act = None
+        new_state = current_state
+        if(act == None):
             return new_state
         
-        if(self.action == 0):
+        if(act == 0):
             new_state -= 5
-        elif(self.action == 1):
+        elif(act == 1):
             if(new_state%5 != 4):
                 new_state += 1
-        elif(self.action == 2):
+        elif(act == 2):
             new_state += 5
         else:
             if(new_state%5 != 0):
                 new_state -= 1
         if(new_state < 0 or new_state > self.end_state or new_state in self.obstacles):
-            new_state = self.state
+            new_state = current_state
         return new_state
     
-    def get_reward(self):
+    def get_reward(self, current_state):
         new_reward = 0
-        if(self.state in self.water):
+        if(current_state in self.water):
             new_reward = -10
-        elif(self.state == self.end_state):
+        elif(current_state == self.end_state):
             new_reward = 10
           
         return new_reward
     
     def take_step(self, policy):
         self.action = self.next_action(policy)
-        self.state = self.next_state()
-        self.reward = self.get_reward()
+        self.state = self.next_state(self.state, self.action)
+        self.reward = self.get_reward(self.state)
         return self.state, self.reward
     
     def is_end_state(self):
